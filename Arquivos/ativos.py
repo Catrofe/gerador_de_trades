0137aeefef561c3ffabcbcb895260d846bc62a65
Catrofe <82066310+Catrofe@users.noreply.github.com>
2022-02-05 15:04:06 -0300

    Refatorando ativos e incluindo nova função

--- Arquivos/ativos.py
+++ Arquivos/ativos.py
@@ -3,14 +3,14 @@ Arquivo que inicia ativos.
 """
 
 
-def retorna_ativos_existentes():
-    dados = []
-    with open("dados/ativos.txt", "r") as arquivo:
-        for ativo in arquivo.readlines():
-            dados.append(ativo.replace("\n", ""))
-        return dados
+class Ativos:
+    def retorna_ativos_existentes(self):
+        dados = []
+        with open("dados/ativos.txt", "r") as arquivo:
+            for ativo in arquivo.readlines():
+                dados.append(ativo.replace("\n", ""))
+            return dados
 
-
-dados = retorna_ativos_existentes()
-
-print("BIDI11" in dados)
+    def verifica_se_ativo_existe(self, ativo):
+        dados = Ativos.retorna_ativos_existentes(self)
+        return ativo in dados

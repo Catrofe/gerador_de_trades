9cacb485b41f04911ee17339913dca9bdb0824de
Catrofe <82066310+Catrofe@users.noreply.github.com>
2022-02-07 14:50:41 -0300

    Finalizando gerador de trades

--- Arquivos/gerador.py
+++ Arquivos/gerador.py
@@ -1,26 +1,55 @@
 """
 Arquivo responsável por gerar a trade
 """
+import random
 from datetime import datetime
-from random import random
+from typing import List
 
+from ativos import Ativos
 from db_admin import Trade
 
 
 class TradeGerenciador:
-    def recebe_parametros(self, ativo, repeticao):
-        return ativo, repeticao
+    def recebe_parametros(self):
+        while True:
+            ativo = self.formata_ativo(
+                input(
+                    "Insira o ativo seguindo o seguinte exemplo: BBSE3, ANIM3, LOGN3\n"
+                )
+            )
+            repeticao = self.formata_repeticao(
+                input("Insira um número inteiro informando a quantidade de compras.\n")
+            )
+
+            dados = Ativos()
+            novo_ativo = self.escolhe_aleatorio_ativo(ativo)
+
+            if dados.verifica_se_ativo_existe(novo_ativo):
+                return novo_ativo, repeticao
+            else:
+                print("Ativo errado ou inexistente")
+                continue
 
     def retorna_data_e_hora(self):
         return datetime.now()  # .strftime("%d/%m/%Y %H:%M")
 
     def retorna_numero_aleatorio(self):
-        return round((random() * 100), 2)
+        return round(random.uniform(1.0, 100.0), 2)
 
     def gera_trade(self):
-        ativo, repeticao = self.recebe_parametros("LOGN3", 2)
+        ativos, repeticao = self.recebe_parametros()
 
         for i in range(repeticao):
             data_e_hora = self.retorna_data_e_hora()
             valor = self.retorna_numero_aleatorio()
-            Trade.create(ativo=ativo, criacao_trade=data_e_hora, preco_trade=valor)
+            Trade.create(ativo=ativos, criacao_trade=data_e_hora, preco_trade=valor)
+
+    def formata_ativo(self, ativo: str) -> List:
+        return ativo.replace(" ", "").split(",")
+
+    def formata_repeticao(self, repeticao: str) -> int:
+        return int(repeticao)
+
+    def escolhe_aleatorio_ativo(self, ativo: List) -> str:
+        quantidade = random.randint(1, len(ativo))
+        return ativo[quantidade]
